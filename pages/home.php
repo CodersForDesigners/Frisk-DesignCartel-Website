a73739c37b23538ef5454d453110cb8e04b899d6
Mario Windsor <mario@lazaro.in>
2019-04-29 17:06:36 +0530

    Insert Content and begin Markup process

--- pages/home.php
+++ pages/home.php
@@ -1,7 +1,7 @@
 <?php
 /*
  *
- * This is a sample page you can copy and use as boilerplate for any new page.
+ *	Home Page
  *
  */
 
@@ -15,11 +15,226 @@
 
 
 
-<!-- Sample Page Content -->
-<section class="intro-section">
-	<p>You are at the source.</p>
+<!-- Sample Section -->
+<section class="sample-section">
 </section>
-<!-- END: Sample Page Content -->
+<!-- END: Sample Section -->
+
+
+<!-- Header Section -->
+<section class="header-section">
+</section>
+<!-- END: Header Section -->
+
+
+<!-- Splash Video Section -->
+<section class="splash-video-section">
+	<div class="row">
+		<img src="https://via.placeholder.com/1920x1080">
+	</div>
+</section>
+<!-- END: Splash Video Section -->
+
+
+<!-- Service Section -->
+<section class="service-section">
+	<div class="row">
+		<div class="container">
+			<div class="columns small-12">
+				Inclusive Methods. Exclusive Design.
+				A new-school approach to a timeless practice.
+				<img src="media/home/service.jpg<?php $ver ?>">
+			</div>
+		</div>
+	</div>
+
+	<div class="row">
+		<div class="container">
+			<div class="columns small-12">
+				ARCHITECTURE & INTERIOR DESIGN
+				Our Architecture and Interior Design studio, in business since 2015, is our bread and butter. Led by Kaushik Kumar, we have designed and executed dozens of projects, bringing our own flavour to each one.
+
+				IN-DEPTH RESEARCH & DESIGN
+				Exploring the intersections between art and architecture, our Deep Design studio applies design thinking to domains in and beyond architecture. This allows us to work within the latest industry practices. Chandrakanth GM takes charge of this studio.
+
+				PROJECT MANAGEMENT
+				While we apply our time-tested and watertight project management capabilities to every project, Joshy Thomas and his project management department offer these services as a standalone to other architects or builders as well.
+
+				3D & VR STUDIO
+				This studio provides cutting-edge 3D and Virtual Reality services as an add-on to any existing project. Similar to our Project Management department, this works as a modular plugin to our own projects, or as a direct service to others.  
+			</div>
+		</div>
+	</div>
+
+</section>
+<!-- END: Service Section -->
+
+
+<!-- Portfolio Section -->
+<section class="portfolio-section">
+	<div class="featured-project">
+		<div class="row">
+			<img src="media/home/featured-1.jpg<?php $ver ?>">
+		</div>
+		<div class="row">
+			<div class="container">
+				<div class="columns small-12">
+					We believe in designing the bigger picture.
+					Our birds-eye perspective on planning and execution allows us to get our hands dirty when it comes to design.
+
+					Our independently functioning studios work in tandem to execute a collective vision. We also bring in the right collaborators at the right stage, to do the job right.
+
+					That’s why we call ourselves a cartel.
+					BAU-HOUSE
+					[VIEW PROJECT]
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="row">
+		<div class="container">
+			<div class="columns small-12">
+				Check out our other projects:
+			</div>
+		</div>
+	</div>
+	<div class="row">
+	</div>
+	<div class="block" style="background-image: url('media/home/DesignCartel_Home_4.jpg<?php $ver ?>');">The Floating Table Office</div>
+	<div class="block" style="background-image: url('media/home/DesignCartel_Home_5.jpg<?php $ver ?>');">The Grey Office</div>
+	<div class="block" style="background-image: url('media/home/DesignCartel_Home_6.jpg<?php $ver ?>');">The Red Office</div>
+	<div class="block" style="background-image: url('media/home/DesignCartel_Home_7.jpg<?php $ver ?>');">The Rustic House</div>
+	<div class="block" style="background-image: url('media/home/DesignCartel_Home_8.jpg<?php $ver ?>');">The White Office</div>
+
+</section>
+<!-- END: Portfolio Section -->
+
+
+<!-- About Section -->
+<section class="about-section">
+	<div class="row">
+		<div class="container">
+			<div class="columns small-12">
+				What do you get when you put two architects and an engineer to work in an office that used to be a music studio in the 80s?
+				
+				An architecture and design firm that functions like a well-oiled machine.
+			</div>
+		</div>
+	</div>
+
+	<div class="row">
+		<img src="media/home/about.jpg<?php $ver ?>">
+	</div>
+
+	<div class="row">
+		<div class="container">
+			<div class="columns small-12">
+				We mix and match our in-house departments, trusted collaborators, contractors, and skilful technicians to create a highly efficient assembly chain. Each section plugs in and out at just the right point in the process.
+
+				What brings the studio heads of Design Cartel together is a shared academic and research-based experience in architecture. However, the origin stories are quite diverse. Within this company are a professional musician, a photographer, a DIY enthusiast, avid travellers, and Scotch enthusiasts. Knowledge and experience from these other creative fields trickle into the smallest design nuances.
+			</div>
+		</div>
+	</div>
+
+</section>
+<!-- END: About Section -->
+
+
+<!-- Process Section -->
+<section class="process-section">
+
+	{BACKGROUND: DesignCartel_Home_10.jpg}
+	Here’s a look at how our projects take shape: 
+
+	PROJECT BRIEF
+	We start our project with a deep dive into the collective vision.
+
+	We want to hear stories, discuss ideas, and ascertain the true nature of what we’re setting out to do, over some freshly brewed coffee.
+
+	This journey begins with going into the inspiration for the space, looking at detailed references, and putting a tentative budget together. 
+
+
+	CONCEPTUALISATION
+	Once we have all the information we need and we’ve established our methodology,
+	we get to the drawing board (and sometimes our LEGO blocks).
+
+	This stage is when we sit down with our multi-faceted and experienced collaborators to help finalise the overall project. 
+
+
+	IMPLEMENTATION
+	Once the concepts are green-lit, we will help you create and issue a Work Order,
+	along with a list of all the finalised items that will go into the project.
+
+	A comprehensive project timeline will help everyone keep up with the project status. 
+
+
+	CONSTRUCTION
+	We’re sticklers for detail and love an organised project.
+
+	To ensure everything is running smoothly, we supervise all collaborators, consultants, and labour. We share updates at every stage of the construction process by comparing the completed work with the project timeline.
+
+	Constant reviews and site visits are also done to ensure quality and consistency. 
+
+
+	HANDOVER
+	A final quality check of everything, including a detailed inspection will be done before all the project documents are shared.
+
+	Once all payments and paperwork are closed, the project is then added to our ever-growing portfolio of unique living, working, and commercial experiences. 
+
+</section>
+<!-- END: Process Section -->
+
+
+<!-- Testimonial Section -->
+<section class="testimonial-section">
+</section>
+<!-- END: Testimonial Section -->
+
+
+<!-- Contact Section -->
+<section class="contact-section">
+
+	We’re the only cartel in the world that will tell you our location. 
+	198 Link Road, Seshadripuram
+	Bangalore – 560020
+	Karnataka, India
+
+	+91 98765 43210
+
+	GET IN TOUCH
+	Name*
+	Company
+	Location*
+	Contact Number*
+	Email*
+	Type of Inquiry (drop down: residential, commercial)*
+	How can we help you?
+	[SEND]
+	Thank you! We’ll get back to you shortly.
+
+</section>
+<!-- END: Contact Section -->
+
+
+<!-- Footer Section -->
+<section class="Footer-section">
+
+	https://twitter.com/DesignCartelBlr
+	https://in.linkedin.com/company/designcartel
+	https://www.facebook.com/designcartel.in
+	https://www.instagram.com/designcartelindia/
+	https://www.behance.net/design-cartel
+
+	Architecture | Interior Design | Project Management | Bangalore, India
+
+</section>
+<!-- END: Footer Section -->
+
+
+<!-- Sample Section -->
+<section class="sample-section">
+</section>
+<!-- END: Sample Section -->
 
 
 
---
+++ pages/project.php
@@ -0,0 +1,56 @@
+<?php
+/*
+ *
+ *	Project Page Template
+ *
+ */
+
+// Page-specific preparatory code goes here.
+
+?>
+
+<?php require_once __DIR__ . '/../inc/above.php'; ?>
+
+
+
+
+
+<!-- Sample Section -->
+<section class="sample-section">
+</section>
+<!-- END: Sample Section -->
+
+
+<!-- Project Cover Section -->
+<section class="project-cover-section">
+</section>
+<!-- END: Project Cover Section -->
+
+
+<!-- Project Content Section -->
+<section class="project-content-section">
+
+	The Floating Table Office
+	One of the first projects we designed, which was also one of our most ambitious. The clients were looking for an innovative and unique design that mirrored their cutting edge ethos, and our entire concept was built around the idea of the suspended table. We re-engineered the work tables by cutting out the legs and suspending them from the ceiling, vastly enhancing workplace interactions. 
+
+	The rest of our design was based around this central idea, with spaces taking on their own character, and functions fitting in around this new layout. We managed to use design changes and innovative thinking to create an office that was unique and thought-provoking, all on a conservative budget. 
+
+	FOR:			Lazaro Advertising Pvt. Ltd.
+	AREA:			2500 square feet
+	TYPE:			Office Interiors
+	LOCATION:		Bangalore
+
+</section>
+<!-- END: Project Content Section -->
+
+
+<!-- Project Gallery Section -->
+<section class="project-gallery-section">
+</section>
+<!-- END: Project Gallery Section -->
+
+
+
+
+
+<?php require_once __DIR__ . '/../inc/below.php'; ?>
