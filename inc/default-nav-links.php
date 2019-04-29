9ccc6c6dea4021ddc12784b946d66b918b01e634
Mario Windsor <mario@lazaro.in>
2019-04-29 14:26:54 +0530

    Update default-nav-links.php

--- inc/default-nav-links.php
+++ inc/default-nav-links.php
@@ -6,8 +6,8 @@
  */
 
 return [
-	[ 'label' => 'Sample', 'title' => 'Sample', 'slug' => '/sample/', 'new_tab' => false ],
-	[ 'label' => 'One', 'title' => 'The First', 'slug' => '/one/', 'new_tab' => false ],
-	[ 'label' => 'Two', 'title' => 'The Second', 'slug' => '/two/', 'new_tab' => false ],
-	[ 'label' => 'Background Video', 'title' => 'Video Killed the Radio Star', 'slug' => '/background-video/', 'new_tab' => false ]
+	[ 'label' => 'Sample', 'title' => 'Sample', 'slug' => '/sample', 'new_tab' => false ],
+	[ 'label' => 'One', 'title' => 'The First', 'slug' => '/one', 'new_tab' => false ],
+	[ 'label' => 'Two', 'title' => 'The Second', 'slug' => '/two', 'new_tab' => false ],
+	[ 'label' => 'Background Video', 'title' => 'Video Killed the Radio Star', 'slug' => '/background-video', 'new_tab' => false ]
 ];
