1b4b439210a0aa39620348887b8a21cd21665610
adi <adityabhat@lazaro.in>
2019-05-13 16:11:42 +0530

    BUG : Fix the header markup

--- inc/dc-header.php
+++ inc/dc-header.php
@@ -2,17 +2,17 @@
 <section class="header-section fill-light js_navigation_section"><!-- Add 'hide' class when scrolling down -->
 	<div class="row">
 		<div class="container">
-			<div class="logo columns small-6">
+			<a class="logo columns small-6" href="<?= $baseURL ?>">
 				<img src="media/icons/dc-logo.svg<?php $ver ?>">
-			</div>
+			</a>
 			<div class="navigation columns small-6 js_navigation_menu"><!-- Add 'show' class toggling/when scrolling up -->
 				<div class="nav-tray fill-light">
 					<a class="link button fill-light strong" href="tel:+919876543210">+ 91 98765 43210</a>
-					<a class="link button fill-light" href="<?= $baseUrl ?? '/' ?>#services">Services</a>
-					<a class="link button fill-light" href="<?= $baseUrl ?? '/' ?>#portfolio">Portfolio</a>
-					<a class="link button fill-light" href="<?= $baseUrl ?? '/' ?>#about">About</a>
-					<a class="link button fill-light" href="<?= $baseUrl ?? '/' ?>#process">Process</a>
-					<a class="link button fill-light" href="<?= $baseUrl ?? '/' ?>#contact">Contact Us</a>
+					<a class="link button fill-light" href="<?= $baseURL ?>#services">Services</a>
+					<a class="link button fill-light" href="<?= $baseURL ?>#portfolio">Portfolio</a>
+					<a class="link button fill-light" href="<?= $baseURL ?>#about">About</a>
+					<a class="link button fill-light" href="<?= $baseURL ?>#process">Process</a>
+					<a class="link button fill-light" href="<?= $baseURL ?>#contact">Contact Us</a>
 				</div>
 				<div class="nav-toggle js_nav_toggle">
 					<button class="fill-light">Menu</button>
