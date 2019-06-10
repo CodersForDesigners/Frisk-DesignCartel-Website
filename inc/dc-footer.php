3b0674b779be2096df41eeed03cab7b5996fd33b
Mario Windsor <mario@lazaro.in>
2019-06-10 12:13:19 +0530

    BugFix: Image version number variable syntax

--- inc/dc-footer.php
+++ inc/dc-footer.php
@@ -3,11 +3,11 @@
 	<div class="row">
 		<div class="container">
 			<div class="social columns small-12 medium-3 inline-middle">
-				<a class="inline-top" href="<?php echo getContent( '', 'twitter' ); ?>" target="_blank"><img src="media/icons/icon-twitter.svg<?php $var ?>"></a>
-				<a class="inline-top" href="<?php echo getContent( '', 'linkedin' ); ?>" target="_blank"><img src="media/icons/icon-linkedin.svg<?php $var ?>"></a>
-				<a class="inline-top" href="<?php echo getContent( '', 'facebook' ); ?>" target="_blank"><img src="media/icons/icon-facebook.svg<?php $var ?>"></a>
-				<a class="inline-top" href="<?php echo getContent( '', 'instagram' ); ?>" target="_blank"><img src="media/icons/icon-instagram.svg<?php $var ?>"></a>
-				<a class="inline-top" href="<?php echo getContent( '', 'behance' ); ?>" target="_blank"><img src="media/icons/icon-behance.svg<?php $var ?>"></a>
+				<a class="inline-top" href="<?php echo getContent( '', 'twitter' ); ?>" target="_blank"><img src="media/icons/icon-twitter.svg<?php echo $ver ?>"></a>
+				<a class="inline-top" href="<?php echo getContent( '', 'linkedin' ); ?>" target="_blank"><img src="media/icons/icon-linkedin.svg<?php echo $ver ?>"></a>
+				<a class="inline-top" href="<?php echo getContent( '', 'facebook' ); ?>" target="_blank"><img src="media/icons/icon-facebook.svg<?php echo $ver ?>"></a>
+				<a class="inline-top" href="<?php echo getContent( '', 'instagram' ); ?>" target="_blank"><img src="media/icons/icon-instagram.svg<?php echo $ver ?>"></a>
+				<a class="inline-top" href="<?php echo getContent( '', 'behance' ); ?>" target="_blank"><img src="media/icons/icon-behance.svg<?php echo $ver ?>"></a>
 			</div>
 			<div class="signature columns small-12 medium-6 inline-middle">
 				<div class="label text-center">Architecture | Interior Design | Project Management <span class="hide-for-tablet">|</span> Bangalore, India</div>
